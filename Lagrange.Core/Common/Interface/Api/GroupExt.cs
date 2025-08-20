3a05f33516ae59cb4b0b78b0355e639de97e6534
Desom-fu <desom233@outlook.com>
2025-08-20 12:18:28 +0800

    Update GroupExt.cs

--- Lagrange.Core/Common/Interface/Api/GroupExt.cs
+++ Lagrange.Core/Common/Interface/Api/GroupExt.cs
@@ -93,7 +93,7 @@ public static class GroupExt
     public static Task<bool> SetGroupFilteredRequest(this BotContext bot, BotGroupRequest request, GroupRequestOperate operate, string reason = "")
         => bot.ContextCollection.Business.OperationLogic.SetGroupFilteredRequest(request.GroupUin, request.Sequence, (uint)request.EventType, operate, reason);
 
-    public static Task<bool> SetFriendRequest(this BotContext bot, SetFriendRequestEvent request, bool accept = true)
+    public static Task<bool> SetFriendRequest(this BotContext bot, BotFriendRequest request, bool accept = true)
         => bot.ContextCollection.Business.OperationLogic.SetFriendRequest(request.SourceUid, accept);
 
     public static Task<bool> GroupPoke(this BotContext bot, uint peerUin, uint targetUin)
